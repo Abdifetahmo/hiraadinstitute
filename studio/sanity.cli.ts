28e4ec0e901c8f7edaa6771a65653ccc84bb10af
cody <abd.gg8@gmail.com>
2026-03-03 22:31:17 +0300

    fix: remove invalid sanity cli deployment config

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,12 +5,5 @@ export default defineCliConfig({
     projectId: 'jn5vyvdg',
     dataset: 'production'
   },
-  studioHost: 'hiraad-jn5vyvdg',
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
-    autoUpdates: true,
-  }
+  studioHost: 'hiraad-jn5vyvdg'
 })
